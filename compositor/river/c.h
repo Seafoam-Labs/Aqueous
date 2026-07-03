3a6ab5d6055ab0a1e6e6b02e16d7704e21fa3512
Zoey <zoey.erin.bauer@gmail.com>
2026-07-02 23:26:14 -0400

    Update c.h

--- compositor/river/c.h
+++ compositor/river/c.h
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: © 2020 The River Developers
+// SPDX-FileCopyrightText: © 2026 Seafoam Labs
 // SPDX-License-Identifier: GPL-3.0-only
 
 #include <linux/input-event-codes.h>
